3f7efbf5c42158bf81679f54fdaa87af0c88264b
dangquangdon <dangquangdon@gmail.com>
2025-06-24 14:41:22 +0300

    re-configure wezterm tab navigation

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
-return {
+local config = {
 	font = wezterm.font("Fira Code"),
 	color_scheme = "Seafoam Pastel",
 	font_size = 10,
@@ -11,8 +12,27 @@ return {
 		top = 4,
 		bottom = 1,
 	},
-	enable_tab_bar = false,
+	enable_tab_bar = true,
+	use_fancy_tab_bar = true,
+	tab_max_width = 10,
 	check_for_updates = false,
 	audible_bell = "Disabled",
 	warn_about_missing_glyphs = false,
+	window_frame = {
+		font_size = 10.0,
+		active_titlebar_bg = "#333333",
+		inactive_titlebar_bg = "#333333",
+	},
 }
+
+config.keys = {}
+for i = 1, 9 do
+	-- ALT + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "ALT",
+		action = act.ActivateTab(i - 1),
+	})
+end
+
+return config
